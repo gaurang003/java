cb58c911222fb32240119c949bcffb9cce88c16b
Gaurang Sharma <157728638+gaurang003@users.noreply.github.com>
2026-08-26 20:53:40 +0530

    Added Types of Interface Classes of Collection
    Added comments explaining the Collections framework and its interfaces.

--- Collection.java
+++ Collection.java
@@ -7,6 +7,20 @@ import static org.example.Collection.List.showList;
 
 public class Collection {
 
+    //Method of Collections
+    // : add, size, remove, iterate, addAll, removeAll and clear
+
+    //1. Interface
+    //a. List Interface : ArrayList, LinkList , Vector : and under Vector -> Stack
+    // Vectors are Thread Safe.
+
+    //b. Queue Interface (FIFO) : PriorityQueue, LinkList , Deque : and under Deque -> ArrayDeque
+    // Deque means Double ended Queue.
+
+    //c. Set Interface : HashSet, LinkedHashSet, SortedSet  : and under SortedSet -> TreeSet
+
+    //d. Map Interface : HashMap, LinkedHashMap, HashTable,  SortedMap : and under SortedMap -> TreeMap
+
     public static void show(){
         ArrayList<String> collection = new ArrayList<>();
         Collections.addAll(collection,"a","b","c");
