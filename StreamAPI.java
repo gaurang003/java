a5ebf11d2e54cc7d568609c30a882d2090caf1d1
Gaurang Sharma <157728638+gaurang003@users.noreply.github.com>
2026-08-28 22:41:37 +0530

    Add files via upload
    StreamAPI.

---
+++ StreamAPI.java
@@ -0,0 +1,10 @@
+package org.example.Java8.StreamAPI;
+
+public class StreamAPI {
+//    java.util.stream
+//    Benefits
+//    Less code
+//    Functional programming
+//    Parallel processing
+//    Faster operations
+}
