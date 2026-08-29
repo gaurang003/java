85fc9b7a94d4f505d4f7b013bdf04108c5ee9e9f
Gaurang Sharma <157728638+gaurang003@users.noreply.github.com>
2026-08-29 20:52:59 +0530

    Add files via upload
    Consumer.

---
+++ Consumer.java
@@ -0,0 +1,8 @@
+package org.example.Java8.FunctionalInterfaces;
+
+public class Consumer {
+
+    //Consumes value but returns nothing.
+    //Consumer<String> c = x -> System.out.println(x);
+    //c.accept("Java");
+}
