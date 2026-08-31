9483256c48215e554d4f0ade4b3b23642ca3efe1
Gaurang Sharma <157728638+gaurang003@users.noreply.github.com>
2026-09-01 00:00:09 +0530

    Add files via upload
    TIME

---
+++ TimeAndSpaceComplexity.java
@@ -0,0 +1,21 @@
+package org.example.ApnaCollege.TimeAndSpaceComplexity;
+
+public class TimeAndSpaceComplexity {
+    //Time Complexity
+    //Relation between Input Size & Running Time (Operations)
+    //Time Complexity is proportional to input n
+
+
+    //Time Complexity -> Relation between ["Input Size"] & Running Time ["(Operations)"]
+    //1. Time Complexity : time complexity proportional to input n
+    //2. Time Complexity : A. BEST CASE, B. AVERAGE CASE , C. WORST CASE
+
+    //BASE CASE : 1 position -> [omega(1)]
+    //AVERAGE CASE : (n + 1)/2 -> [theta (n+1)/2]
+    //WORST CASE : proportional to n -> [Big O (n)]
+
+    //Space Complexity -> 1. In Some Case Space Complexity depends on Input Size.
+
+
+
+}
